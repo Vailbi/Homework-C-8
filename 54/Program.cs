6d66efb2ef75934de102416c3a9fc5b86885a115
Fedor <vailbi94@gmail.com>
2022-07-07 15:32:24 +0300

    init commit

---
+++ 54/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int [,] matrix = new int[5,5];
+
+void FillArray(int [,] matr) // заполнение матрицы случайными числами
+{
+    for (int i = 0; i < matr.GetLength(0); i++)  
+    {   
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0,10);
+            
+        }
+    }
+}
+
+FillArray(matrix);
+
+void PrintArray(int [,] matr) // печать матрицы
+{
+    for (int i = 0; i < matr.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+        Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(matrix);
+
+
+void SortMatrix(int [,] matr) // сортировка строк по убыванию
+{
+int temp = 0;
+int step = 0;
+while (step<=matr.GetLength(1))
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1)-1; j++) 
+        {
+            if (matr[i,j]<matr[i,j+1] )
+            {
+                temp = matr[i,j];
+                matr[i,j] = matr[i,j+1];
+                matr[i,j+1] = temp;
+            }
+        }
+    }
+step++;   
+}
+    PrintArray(matr);
+}
+
+Console.WriteLine();
+Console.WriteLine("Отсартированная матрица");
+SortMatrix(matrix);
+
+Console.WriteLine();
---
+++ 62/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 62: Заполните спирально массив 4 на 4.
+
+int [,] matrix = new int [4,4];
+int num = 1;
+int row=0;
+int col=0;
+void FillImage (int row, int col) // заполнение первой ячейка
+{
+    matrix[row,col] = num;
+    num++;
+}
+void FillMatrixRight (int row, int col) // движение вправо
+{
+    matrix[row,col+1] = num;
+    num++;
+}
+void FillMatrixDown (int row, int col) // движение вниз
+{
+    matrix[row+1,col] = num;
+    num++;
+}
+void FillImgLeft (int row, int col) // движение влево
+{
+    matrix[row,col-1] = num;
+    num++;
+}
+void FillImgUp (int row, int col) // движение вверх
+{
+    matrix[row-1,col] = num;
+    num++;
+}
+
+void PrintArray(int [,] matr) // печать матрицы
+{
+    for (int i = 0; i < matr.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+        Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int step = matrix.GetLength(0)*matrix.GetLength(1);
+
+FillImage(row,col);
+
+for (int i = 0; i < step; i++) //цикл для заполнения матрицы
+{
+    if (col<matrix.GetLength(1)-1 && matrix[row,col+1]==0)
+    {
+        FillMatrixRight(row,col);
+        col++;
+    }
+    else if (row<matrix.GetLength(0)-1 && matrix[row+1,col]==0)
+    {
+        FillMatrixDown(row,col);
+        row++;
+    }
+    else if (col>0 && matrix[row,col-1]==0)
+    {
+        FillImgLeft(row,col);
+        col--;
+    }
+    else while (row>0 && matrix[row-1,col]==0)
+    {
+        FillImgUp(row,col);
+        row--;
+    }
+}
+
+PrintArray(matrix);
